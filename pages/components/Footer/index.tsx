dd6a606f2e809f7556ca536d86f279e67a2fc2d4
ezequiassantos11 <ezequiasmg16@gmail.com>
2022-02-08 16:15:26 -0300

    mais algumas partes da barra de player prontas

--- pages/components/Footer/index.tsx
+++ pages/components/Footer/index.tsx
@@ -1,8 +1,55 @@
-import styles from "./styles.module.scss";
+import {useState} from "react";
+import Styles from "./styles.module.scss";
+import Replay5Icon from '@mui/icons-material/Replay5';
+import Forward5Icon from '@mui/icons-material/Forward5';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+
 export function Footer(){
+  const [onPlay, setOnPlay] = useState(false);
   return(
-    <footer className={styles.container}>
-      ezequias santos soares
+    <footer className={Styles.container}>
+     <div className={Styles.wrapper}>
+       <div className={Styles.preview}>
+        <img src="assets/music.webp" alt="" />
+        <div className={Styles.text}>
+          <h3>nome da música</h3>
+          <p>Studio produsiu</p>
+        </div>
+        <a>+</a>
+       </div>
+       <div className={Styles.navigation}>
+          <div className={Styles.iconsPlayer}>
+            <Replay5Icon className={Styles.iconPlay}/>
+
+            <SkipPreviousIcon className={Styles.iconPlay}/>
+
+            <PlayCircleOutlineIcon 
+              onClick={()=>setOnPlay(true)}  
+              className={Styles.iconPlay}
+              style={{display: onPlay ? "none" : "flex"}}
+            />
+
+            <PauseCircleOutlineIcon onClick={()=>setOnPlay(false)} style={{display: onPlay ? "flex" : "none"}} className={Styles.iconPlay}/>
+
+            <SkipNextIcon className={Styles.iconPlay}/>
+
+            <Forward5Icon className={Styles.iconPlay}/>
+          </div> 
+          <div className={Styles.progress}>
+            <span>2:00</span>
+            <span className={Styles.line}></span>
+            <span>3:00</span>
+          </div>
+       </div>
+       <div className={Styles.controllers}>
+          <VolumeUpIcon/>
+          <span></span>
+       </div>
+     </div>
     </footer>
   )
 }
--- pages/components/Header/index.tsx
+++ pages/components/Header/index.tsx
@@ -6,31 +6,33 @@ import Styles from './styles.module.scss';
 export function Header({blackHeader}){
   const [navOpen, setNavOpen] = useState(false);
   return(
-    <header className={`${blackHeader ? `${Styles.containerBlack}` : `${Styles.container}` }`}>
-      <div className={Styles.left}>
-        <button className={Styles.arrowLeft}>
-          <ArrowBackIosIcon />
-        </button>
-        <button className={Styles.arrowRight}>
-          <ArrowBackIosIcon />
-        </button>
-      </div>
-        <button>Ativar</button>
-      <div className={Styles.right}>
-        <div className={Styles.user} onClick={()=> setNavOpen(!navOpen)}>
-          <img src="https://github.com/ezequiassantos11.png" alt="" />
-          <h1>Nome de usuario</h1>
-          <ArrowDropDownIcon/>
+    <header className={Styles.container}>
+      <div className={`${Styles.initial} + ${blackHeader ? `${Styles.wrapperBlack}` : ""}`}>
+        <div className={Styles.left}>
+          <button className={Styles.arrowLeft}>
+            <ArrowBackIosIcon />
+          </button>
+          <button className={Styles.arrowRight}>
+            <ArrowBackIosIcon />
+          </button>
         </div>
-        <nav className={`${navOpen ? `${Styles.navOpen}` : `${Styles.navClose}`}`}>
-          <ul>
-            <li>Conta</li>
-            <li>Perfil</li>
-            <li>Premium</li>
-            <li>Baixar</li>
-            <li>Sair</li>
-          </ul>
-        </nav>
+          <h1>Ezequias Web-Developer</h1>
+        <div className={Styles.right}>
+          <div className={Styles.user} onClick={()=> setNavOpen(!navOpen)}>
+            <img src="https://github.com/ezequiassantos11.png" alt="" />
+            <h1>Nome de usuario</h1>
+            <ArrowDropDownIcon/>
+          </div>
+          <nav className={`${navOpen ? `${Styles.navOpen}` : `${Styles.navClose}`}`}>
+            <ul>
+              <li>Conta</li>
+              <li>Perfil</li>
+              <li>Premium</li>
+              <li>Baixar</li>
+              <li>Sair</li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   )
